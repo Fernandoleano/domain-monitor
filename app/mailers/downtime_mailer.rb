c2fec3a0502437dae7c058c93012ecba594283c4
FernandoLeano <fernandoleano4@gmail.com>
2025-12-17 19:57:55 -0500

    Fix: Add production cable DB config and fix Mailer argument errors

--- app/mailers/downtime_mailer.rb
+++ app/mailers/downtime_mailer.rb
@@ -1,15 +1,15 @@
 class DowntimeMailer < ApplicationMailer
   default from: "alerts@domainmonitor.com"
 
-  def down_alert(user, site)
-    @user = user
-    @site = site
+  def down_alert
+    @user = params[:user]
+    @site = params[:site]
     mail(to: @user.email_address, subject: "🔴 Alert: #{@site.name} is DOWN")
   end
 
-  def up_alert(user, site)
-    @user = user
-    @site = site
+  def up_alert
+    @user = params[:user]
+    @site = params[:site]
     mail(to: @user.email_address, subject: "🟢 Recovered: #{@site.name} is back UP")
   end
 end

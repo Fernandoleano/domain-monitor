cdc14a5ec5be5416dd0ea4a6d36f8263896619fa
FernandoLeano <fernandoleano4@gmail.com>
2025-12-17 19:43:11 -0500

    Feat: Add premium marketing landing page with custom layout

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -1,6 +1,7 @@
 class MarketingController < ApplicationController
     # allow unauthenticated users to access the index action
     allow_unauthenticated_access
+    layout "marketing"
 
   def index
     redirect_to sites_path if authenticated?
